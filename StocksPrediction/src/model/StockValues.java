051b52fb8780a1f17fecf15a1c801bcb01c973bf
Lucas <Lucas@Lucas>
2016-09-12 00:15:14 -0300

    Criando conceitos basicos de companhia e iniciando seleção de empresas que serão utilizadas para os testes basicos.

---
+++ StocksPrediction/src/model/StockValues.java
@@ -0,0 +1,78 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package stocksprediction;
+
+import java.math.BigDecimal;
+import java.util.Calendar;
+
+/**
+ *
+ * @author Lucas
+ */
+ public class StockValues {
+
+        private Calendar date;
+
+        private BigDecimal open;
+        private BigDecimal low;
+        private BigDecimal high;
+        private BigDecimal close;
+
+        //private BigDecimal adjClose;
+
+        private Long volume;
+
+        public Calendar getDate() {
+            return date;
+        }
+
+        public void setDate(Calendar date) {
+            this.date = date;
+        }
+
+        public BigDecimal getOpen() {
+            return open;
+        }
+
+        public void setOpen(BigDecimal open) {
+            this.open = open;
+        }
+
+        public BigDecimal getLow() {
+            return low;
+        }
+
+        public void setLow(BigDecimal low) {
+            this.low = low;
+        }
+
+        public BigDecimal getHigh() {
+            return high;
+        }
+
+        public void setHigh(BigDecimal high) {
+            this.high = high;
+        }
+
+        public BigDecimal getClose() {
+            return close;
+        }
+
+        public void setClose(BigDecimal close) {
+            this.close = close;
+        }
+
+        public Long getVolume() {
+            return volume;
+        }
+
+        public void setVolume(Long volume) {
+            this.volume = volume;
+        }
+        
+        
+
+    }
