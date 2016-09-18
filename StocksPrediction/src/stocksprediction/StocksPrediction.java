4ee1f25057088a9ca4f67bb65305a79e31016afb
Lucas <Lucas@Lucas>
2016-09-18 18:50:03 -0300

    Implemetação de indicadores técnicos SMA e EMA

--- StocksPrediction/src/stocksprediction/StocksPrediction.java
+++ StocksPrediction/src/stocksprediction/StocksPrediction.java
@@ -12,18 +12,33 @@ import treinamentoredes.TreinamentoRedes;
  *
  * @author Lucas
  */
-
 public class StocksPrediction {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) throws IOException {
-        
-        
-        TreinamentoRedes t = new TreinamentoRedes();
 
-        
+        TreinamentoRedes t = new TreinamentoRedes();
+//        com.tictactec.ta.lib.Core c = new com.tictactec.ta.lib.Core();
+//        double[] ddd = {3, 10, 10, 10, 1, 10, 10, 10};
+//        MInteger outBagIdx = new MInteger();
+//        MInteger outNBElement = new MInteger();                
+//        double[] outReal = new double[60];
+//        
+//        c.movingAverage(0, (ddd.length - 1), ddd, 2, MAType.Tema, outBagIdx, outNBElement, outReal);
+//        System.out.println(ddd.length);
+//        System.out.println(outReal.length);
+//        System.out.println("BAG " + outBagIdx.value);
+//        System.out.println(outNBElement.value);
+//        int n = 0;
+//        for (double d : outReal) {
+//            n++;
+//            System.out.print("id: " + n + " ");
+//            System.out.println(d);
+//            
+//        }
+
     }
-    
+
 }
--- StocksPrediction/src/treinamentoredes/TreinamentoRedes.java
+++ StocksPrediction/src/treinamentoredes/TreinamentoRedes.java
@@ -5,17 +5,41 @@
  */
 package treinamentoredes;
 
+import com.sun.media.jfxmedia.logging.Logger;
+import eu.verdelhan.ta4j.Decimal;
+import eu.verdelhan.ta4j.Indicator;
+import eu.verdelhan.ta4j.Tick;
+import eu.verdelhan.ta4j.TimeSeries;
+import eu.verdelhan.ta4j.indicators.simple.ClosePriceIndicator;
+import eu.verdelhan.ta4j.indicators.trackers.EMAIndicator;
 import java.io.IOException;
 import java.util.Calendar;
 import java.util.List;
 import java.util.Map;
-import java.util.Vector;
 import java.util.logging.Level;
 import model.Company;
 import model.ListaEmpresas;
 import yahoofinance.Stock;
 import yahoofinance.YahooFinance;
 import yahoofinance.histquotes.Interval;
+import java.math.BigDecimal;
+import java.util.Vector;
+import yahoofinance.histquotes.HistoricalQuote;
+import eu.verdelhan.ta4j.indicators.trackers.SMAIndicator;
+import java.text.SimpleDateFormat;
+import java.time.Period;
+import java.util.ArrayList;
+import java.util.Date;
+import org.joda.time.DateTime;
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.axis.DateAxis;
+import org.jfree.chart.plot.XYPlot;
+import org.jfree.data.time.Day;
+import org.jfree.data.time.TimeSeriesCollection;
+import org.jfree.ui.ApplicationFrame;
+import org.jfree.ui.RefineryUtilities;
 
 /**
  *
@@ -28,15 +52,17 @@ public class TreinamentoRedes {
     private static List<Company> empresas;
 
     public TreinamentoRedes() throws IOException {
-        
-        ta-lib
+
+//c.macd(0, 0, inReal, 0, 0, 0, outBegIdx, outNBElement, outMACD, outMACDSignal, outMACDHist)
+//c.rsi(0, 0, inReal, 0, outBegIdx, outNBElement, outReal)
+//c.obv(0, 0, inReal, inVolume, outBegIdx, outNBElement, outReal)
         empresas = new Vector<>();
 
         dataInicialTreinamento = Calendar.getInstance();
-        dataInicialTreinamento.set(14, 1, 1);
+        dataInicialTreinamento.set(16, 2, 6);
 
         dataFinalTreinamento = Calendar.getInstance();
-        dataFinalTreinamento.set(15, 1, 1);
+        dataFinalTreinamento.set(16, 8, 18);
 
         this.carregaDadosHistoricosParaTreinamento();
 
@@ -46,20 +72,91 @@ public class TreinamentoRedes {
 
         //yahoofinance.Utils.
         YahooFinance.logger.setLevel(Level.OFF);
-        System.out.println(YahooFinance.CONNECTION_TIMEOUT);
-        Map<String, Stock> consulta = YahooFinance.get(ListaEmpresas.getListaDeEmpresas(), dataInicialTreinamento, dataFinalTreinamento, Interval.DAILY);
+        Stock consulta = YahooFinance.get("PETR4.SA", dataInicialTreinamento, dataFinalTreinamento, Interval.DAILY);
+//        Map<String, Stock> consulta = YahooFinance.get("PETR4.SA", dataInicialTreinamento, dataFinalTreinamento, Interval.WEEKLY);
 
-        for (Map.Entry<String, Stock> entry : consulta.entrySet()) {
-            String simbolo = entry.getKey();
-            Stock value = entry.getValue();
-            
-            Company c = new Company(simbolo);
-            
-            c.setHistorico(value.getHistory());            
-            empresas.add(c);
-            
+        //for (Map.Entry<String, Stock> entry : consulta.entrySet()) {
+//            String simbolo = entry.getKey();
+//            Stock value = entry.getValue();
+        Company c = new Company("PETR4");
+
+        //System.out.println(consulta.getHistory().t());
+        c.setHistorico(consulta.getHistory());
+        empresas.add(c);
+
+        ArrayList<Tick> serieTemporal = new ArrayList<>();
+        for (int j = c.getHistorico().size() - 1; j > 0; j--) {
+            HistoricalQuote hq = c.getHistorico().get(j);
+
+            DateTime d = new DateTime(hq.getDate().getTimeInMillis());
+//            Period periodo = new org.joda.time.Period
+            Tick dado = new Tick(d, hq.getOpen().doubleValue(), hq.getHigh().doubleValue(), hq.getLow().doubleValue(), hq.getClose().doubleValue(), hq.getVolume());
+            //dado.
+            serieTemporal.add(dado);
+            System.out.println(hq.toString());
         }
+
+        TimeSeries serie = new TimeSeries(serieTemporal);
+        ClosePriceIndicator closePrice = new ClosePriceIndicator(serie);
+        EMAIndicator ema = new EMAIndicator(closePrice, 4);
+        SMAIndicator sma = new SMAIndicator(closePrice, 4);
+        SMAIndicator sma9 = new SMAIndicator(closePrice, 9);
+        SMAIndicator sma18 = new SMAIndicator(closePrice, 18);
+        //sma.c;
+
+        TimeSeriesCollection dataset = new TimeSeriesCollection();
+        dataset.addSeries(buildChartTimeSeries(serie, closePrice, "Petrobras (PETR4) - Close Price"));
+        dataset.addSeries(buildChartTimeSeries(serie, sma, "Média móvel Simples - 4 dias"));
+        dataset.addSeries(buildChartTimeSeries(serie, sma9, "Média móvel Simples - 9 dias"));
+        dataset.addSeries(buildChartTimeSeries(serie, sma18, "Média móvel Simples - 18 dias"));
+
+        /**
+         * Creating the chart
+         */
+        JFreeChart chart = ChartFactory.createTimeSeriesChart(
+                "Petrobras (PETR4) - Close Price", // title
+                "Date", // x-axis label
+                "Price Per Unit", // y-axis label
+                dataset, // data
+                true, // create legend?
+                true, // generate tooltips?
+                false // generate URLs?
+        );
+        XYPlot plot = (XYPlot) chart.getPlot();
+        DateAxis axis = (DateAxis) plot.getDomainAxis();
+        axis.setDateFormatOverride(new SimpleDateFormat("yyyy-MM-dd"));
+
         
-        ta.
+        displayChart(chart);
+
+        System.out.println(sma.getValue(serie.getTickCount() - 1));
+        System.out.println(sma9.getValue(serie.getTickCount() - 1));
+        System.out.println(sma18.getValue(serie.getTickCount() - 1));
+        System.out.println(ema.getValue(serie.getTickCount() - 1));
+
+//        }
+    }
+
+    private static org.jfree.data.time.TimeSeries buildChartTimeSeries(TimeSeries tickSeries, Indicator<Decimal> indicator, String name) {
+        org.jfree.data.time.TimeSeries chartTimeSeries = new org.jfree.data.time.TimeSeries(name);
+        for (int i = 0; i < tickSeries.getTickCount(); i++) {
+            Tick tick = tickSeries.getTick(i);
+            chartTimeSeries.add(new Day(tick.getEndTime().toDate()), indicator.getValue(i).toDouble());
+        }
+        return chartTimeSeries;
+    }
+
+    private static void displayChart(JFreeChart chart) {
+        // Chart panel
+        ChartPanel panel = new ChartPanel(chart);
+        panel.setFillZoomRectangle(true);
+        panel.setMouseWheelEnabled(true);
+        panel.setPreferredSize(new java.awt.Dimension(500, 270));
+        // Application frame
+        ApplicationFrame frame = new ApplicationFrame("Exemplo de Indicadores - PETR4");
+        frame.setContentPane(panel);
+        frame.pack();
+        RefineryUtilities.centerFrameOnScreen(frame);
+        frame.setVisible(true);
     }
 }
